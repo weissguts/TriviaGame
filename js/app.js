e5ad8ca225d0d6f0dbebfa3f8c0095e92fef322d
Daniel Hernandez <weissguts@gmail.com>
2017-12-10 15:20:52 -0700

    JS almost done.. i hope

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 var timeStart = 30;
 var timerElem = document.getElementById('countdownId');
 var timerId = setInterval(countdown, 1000);
+var correct = 0;
+var incorrect = 0;
+var finGame = document.getElementById('finished-game');
 
 $( document ).ready(function() {
     $('#start-game-main').hide();
+    // $('#finished-game').hide();
 });
 
 
@@ -15,12 +19,41 @@ $('#game-start').click(function () {
 });
 
 function countdown() {
-    if (timeStart == 0) {
+    if (timeStart == 20) {
         clearTimeout(timerId);
-        //dosomething
+        finGame.innerHTML = '<h3>Correct</h3> ' + correct + '<br>' + '<h3>Incorrect</h3> ' + incorrect;
+
     } else {
         timerElem.innerHTML = timeStart + ' seconds remaining';
         timeStart--;
+
+        finGame.innerHTML = '<h3>Correct</h3> ' + correct + '<br>' + '<h3>Incorrect</h3> ' + incorrect;
     }
 
 };
+
+function answers() {
+    if (document.getElementById('Austin').checked==true) {
+        correct++;
+    } else {
+        incorrect++;
+    }
+
+    if (document.getElementById('Sacramento').checked==true) {
+        correct++;
+    } else {
+        incorrect++;
+    }
+
+    if (document.getElementById('SantaFe').checked==true) {
+        correct++;
+    } else {
+        incorrect++
+    }
+
+    if (document.getElementById('Tucson').checked==true) {
+        correct++;
+    } else {
+        incorrect++;
+    }
+}
